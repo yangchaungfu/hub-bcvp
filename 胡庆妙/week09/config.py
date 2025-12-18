0720efce21749487ddfeccda957a58811e2d6604
林中漫步 <23116851@qq.com>
2025-12-19 02:14:23 +0800

    # 在本文中标注预测出来的实体，输出标注后的文本
    Updated validation data path and adjusted sentence length and number of epochs.

--- 胡庆妙/week09/config.py
+++ 胡庆妙/week09/config.py
@@ -11,13 +11,14 @@ Config = {
 
     "schema_path": "ner_data/schema.json",
     "train_data_path": "ner_data/train.txt",
-    "valid_data_path": "ner_data/test.txt",
+    "valid_data_path": "ner_data/valid.txt",
+    "test_data_path": "ner_data/test.txt",
 
-    "sentence_len": 80,
+    "sentence_len": 90,
     "embed_size": 768,
     "class_num": 9,
 
-    "num_epochs": 10,  # 训练轮数
+    "num_epochs": 15,  # 训练轮数
     "batch_size": 16,  # 每批的训练样本数
     "learning_rate": 0.0004,
     "optimizer": "adam",
