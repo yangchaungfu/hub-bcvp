eb803b589f6adf9b13278f1ee341960df1131b25
DK-kk1 <18338815893@163.com>
2025-12-19 15:02:56 +0800

    Create config.py with training parameters
    Add configuration parameters for model training.

---
+++ 蒋珂仡/week09/config.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+配置参数信息
+"""
+'''
+
+关于transformers自带的序列化工具
+模型文件下载 https://huggingface.co/models
+
+'''
+
+Config = {
+    "model_path": "model_output",
+    "schema_path": "ner_data/schema.json",
+    "train_data_path": "ner_data/train",
+    "valid_data_path": "ner_data/test",
+    "vocab_path":"chars.txt",
+    "max_length": 100,
+    "hidden_size": 256,
+    "num_layers": 2,
+    "epoch": 20,
+    "batch_size": 16,
+    "optimizer": "adam",
+    "learning_rate": 1e-3,
+    "use_crf": True,
+    "class_num": 9,
+    "bert_path": "google-bert/bert-base-chinese",
+    "input_dim": 768
+
+}
+
