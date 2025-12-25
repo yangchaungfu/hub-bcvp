b29e73ca97d36cbf82aa12ed2df1486389c92677
林中漫步 <23116851@qq.com>
2025-12-25 10:41:14 +0800

    改用 tokenizer.vocab.get，修改输出序列的长度与输入字符数不对应的问题。

--- 胡庆妙/week10/text_generation_by_bert.py
+++ 胡庆妙/week10/text_generation_by_bert.py
@@ -1,4 +1,5 @@
 # coding:utf8
+import time
 
 import torch
 import torch.nn as nn
@@ -31,18 +32,13 @@ class LanguageModel(nn.Module):
             attention_mask: shape: [batch_size, sen_len, sen_len]
             target: shape: [batch_size, sen_len]
         """
-        # print(">> input_ids:", input_ids)
-        # print(">> attention_mask:", attention_mask)
-        # print(">> target:", target)
         # [batch_size, sen_len] -> [batch_size, sen_len, input_dim]
         sequence_out, _ = self.bert(input_ids=input_ids, attention_mask=attention_mask, return_dict=False)
 
         # [batch_size, sen_len, input_dim] -> [batch_size, sen_len, vocab_size]
         logits = self.classify(sequence_out)
         if target is not None:
-            loss = self.loss(logits.view(-1, logits.shape[-1]), target.view(-1))
-            print(">> loss: ", loss)
-            return loss
+            return self.loss(logits.view(-1, logits.shape[-1]), target.view(-1))
         else:
             return torch.softmax(logits, dim=-1)
 
@@ -52,7 +48,6 @@ def load_corpus(path):
     with open(path, encoding="utf8") as f:
         for line in f:
             corpus += line.strip()
-            corpus += "\n"
     return corpus
 
 
@@ -62,12 +57,11 @@ def build_sample(corpus, window_size):
     j = i + window_size
     input_text = corpus[i:j]
     target_text = corpus[i + 1:j + 1]  # 输入输出错开一位
-
-    input_ids = encode_sentence(input_text, window_size)
-    target = encode_sentence(target_text, window_size)
+    input_ids = encode_sentence(input_text)
+    target = encode_sentence(target_text)
     # 用下三角矩阵做掩码，使得bert在处理第i个token时只有第i个token左侧的token是可见的
     # 原理：attention_scores = attention_scores + (1.0 - attention_mask) * -10000.0
-    attention_mask = np.tril(np.ones((window_size, window_size), dtype=np.int32)).tolist()
+    attention_mask = np.tril(np.ones((len(input_ids), len(input_ids)), dtype=np.int32)).tolist()
     return input_ids, attention_mask, target
 
 
@@ -84,15 +78,16 @@ def build_dataset(sample_num, corpus, sentence_len):
     return torch.LongTensor(ds_input_ids), torch.LongTensor(ds_atten_mask), torch.LongTensor(ds_target)
 
 
-def encode_sentence(sentence, sentence_len, truncation=True):
-    input_ids = tokenizer.encode(
-        sentence
-        , max_length=sentence_len
-        , padding="max_length"
-        , truncation=truncation
-        , add_special_tokens=False
-        , return_tensors=None  # "pt": 返回tensor, "None": 返回list
-    )
+def encode_sentence(sentence):
+    # 这里使用tokenizer.encode()会有问题：输出序列的长度与输入字符数不对应
+    # input_ids = tokenizer.encode(
+    #     sentence
+    #     , add_special_tokens=False
+    #     , return_tensors=None  # "pt": 返回tensor, "None": 返回list
+    # )
+    input_ids = []
+    for char in sentence:
+        input_ids.append(tokenizer.vocab.get(char, tokenizer.vocab["[UNK]"]))
     return input_ids
 
 
@@ -102,10 +97,10 @@ def generate_sentence(openings, sentence_len, model):
     with torch.no_grad():
         pred_char = ""
         # 如果生成了换行符 或 生成的文本超过限制， 则终止迭代
-        while pred_char != "\n" and len(openings) <= 70:
+        while pred_char != "\n" and len(openings) <= 60:
             openings += pred_char
             # 从openings的尾部截取长度为sentence_len的字符串
-            input_ids = encode_sentence(openings[-sentence_len:], sentence_len, truncation=False)
+            input_ids = encode_sentence(openings[-sentence_len:])
             input_ids = torch.LongTensor([input_ids])
             attention_mask = torch.ones((len(input_ids), len(input_ids)), device=input_ids.device).unsqueeze(0)
             if torch.cuda.is_available():
@@ -133,12 +128,12 @@ def sampling_strategy(prob_distribution):
 
 
 def train(corpus_path, save_weight=True):
-    epoch_num = 16  # 训练轮数
+    epoch_num = 10  # 训练轮数
     batch_size = 50  # 每次训练样本个数
-    train_sample_num = 40000  # 每轮训练总共训练的样本总数
-    learning_rate = 0.001  # 学习率
+    train_sample_num = 10000  # 每轮训练总共训练的样本总数
+    learning_rate = 0.0004  # 学习率
 
-    sentence_len = 40  # 样本文本长度
+    sentence_len = 30  # 样本文本长度
     embed_size = 768  # bert默认词向量维度
 
     corpus = load_corpus(corpus_path)  # 加载语料
@@ -147,11 +142,13 @@ def train(corpus_path, save_weight=True):
         model = model.cuda()
     optim = torch.optim.Adam(model.parameters(), lr=learning_rate)  # 建立优化器
 
-    print("开始训练...")
+    begin_time = time.time()
+    print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} 开始训练...")
     for epoch in range(epoch_num):
         model.train()
         watch_loss = []
-        for batch in range(int(train_sample_num / batch_size)):
+        batch_num = int(train_sample_num / batch_size)
+        for batch_idx in range(batch_num):
             input_ids, atten_mask, target = build_dataset(batch_size, corpus, sentence_len)  # 构建一组训练样本
             if torch.cuda.is_available():
                 input_ids, atten_mask, target = input_ids.cuda(), atten_mask.cuda(), target.cuda()
@@ -161,13 +158,18 @@ def train(corpus_path, save_weight=True):
             loss.backward()  # 计算梯度
             optim.step()  # 更新权重
             watch_loss.append(loss.item())
+            if (batch_idx + 1) % int(batch_num / 10) == 0:  # 每轮训练到1/10时，就输出loss看看
+                print("\tbatch loss %.4f" % loss.item())
 
         print("=========\n第%d轮 的平均loss: %f" % (epoch + 1, np.mean(watch_loss)))
         print(generate_sentence("让他在半年之前，就不能做出", sentence_len, model))
         print(generate_sentence("李慕站在山路上，深深的呼吸", sentence_len, model))
 
+    spent_time = round((time.time() - begin_time), 2)  # 训练及验证耗时
+    print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} 训练结束, 耗时: {spent_time}秒")
+
     if save_weight:
-        torch.save(model.state_dict(), "text_generation_by_bert.py")
+        torch.save(model.state_dict(), "text_generation_by_bert.pth")
         return
 
 
